4752f7d86c631cf19d6b0303c2130676d5f8339f
CaioAlmeida09 <Caio09092001@gmail.com>
2023-04-05 21:59:18 -0300

    Fazendo as interações com as imagens

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const buttonclick = document.getElementById("mybutton")
-const dolar = 5.2
+const select = document.getElementById("mySelct")
+const img = document.getElementById("bandeira")
+const dolar = 5.05
+const Euro = 5.49
+const Bit = 141.585
 const buttonPosClick = () => {
     const input = document.getElementById("myInput").value
     const valueFinal = input / dolar
@@ -8,13 +12,46 @@ const buttonPosClick = () => {
         style: "currency",
         currency: "BRL",
     }).format(input)
-    
-    const SubstDolar = document.getElementById("valorinDolar")
-    SubstDolar.innerText = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    }).format(valueFinal.toFixed(2))
+
+    if (select.value === "US$ Dolar Americano") {
+        const SubstExt = document.getElementById("valorExt")
+        SubstExt.innerText = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(valueFinal.toFixed(2))
+    }
+
+    if (select.value === "€ Euro") {
+        const SubstExt = document.getElementById("valorExt")
+        SubstExt.innerText = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format((input / Euro).toFixed(2))
+    }
+
+    else {
+        const SubstExt = document.getElementById("valorExt")
+        SubstExt.innerText = `₿ ${(input / 141.585).toFixed(5)}`
+    }
+}
+
+const testSelect = () => {
+    const textMoeda = document.getElementById("textMoeda")
+    if (select.value === "€ Euro") {
+        textMoeda.innerText = "€ Euro"
+        img.src = "/assets/Euro.svg"
+
+    }
+    else if (select.value === "₿ Bitcoin") {
+        textMoeda.innerText = "₿ Bitcoin"
+        img.src = "/assets/Bitcoin.png"
+    }
+    else if (select.value === "US$ Dolar Americano") {
+        textMoeda.innerText = "US$ Dolar Americano"
+        img.src = "/assets/Eua.svg"
+    }
 
 }
-buttonclick.addEventListener("click", buttonPosClick)
 
+buttonclick.addEventListener("click", buttonPosClick)
+select.addEventListener("change", testSelect)
